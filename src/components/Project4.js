23a16a0c787e3088e1359be19af1287e0b0ddc12
AnkitShrivastava27 <ankitshrivastavaks@gmail.com>
2026-04-02 14:08:45 +0530

    april changes

--- src/components/Project4.js
+++ src/components/Project4.js
@@ -44,7 +44,7 @@ const Project1 = () => {
         </ul>
         <ul>
             <h5>Links</h5>
-            <li>Website : <a href="#">Not Deployed</a></li>
+            <li>Website : Not Deployed</li>
             <li>github repo : <a href="https://github.com/AnkitShrivastava27">Click here</a></li>
         </ul>
         </div>
